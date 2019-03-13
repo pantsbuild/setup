7688a3385b90e81cafc41c9b28ffe8d1a8d06f19
Eric Arellano <ericarellano@me.com>
2019-03-12 17:34:14 -0700

    Add ci.py helper script to allow parametrizing the pants_version (#33)
    ### Problem
    The `./pants` script claims to support running with both a pinned `pants_version` in the `pants.ini` and with the `pants_version` unspecified.
    
    We currently do not test this unspecified branch, which we should be doing to guarantee that functionality always works.
    
    Further, this is pre-work for https://github.com/pantsbuild/setup/pull/32, which will add `--python-version` as another parameter for `ci.py` to allow an unspecified Python version vs. `pants.ini` specifying 2.7 or 3.6 (FYI resulting [`.travis.yml`](https://github.com/pantsbuild/setup/pull/32/files#diff-354f30a63fb0907d4ad57269548329e3)).
    
    ### Solution
    Introduce a new `ci.py` helper script that uses argparse to have the parameter `--pants-version {unspecified,config}`.
    
    #### Requires adding Python 3 to CI shards
    Because this script uses modern Python 3—i.e. enums, f-strings, type hints, keyword-only args, and `subprocess.run()`—it requires Python 3.5+ to run.
    
    Even though we can't yet run `./pants` with Python 3 until https://github.com/pantsbuild/setup/pull/32 lands, this PR does some of the pre-work in adding Python 3 to each of the shards so that we can run this script. This is valid to do because every single shard is going to run the tests with both Py27 and Py36—unlike Pants where we have dedicated shards for each interpreter—so there is only benefit in ensuring each shard has Py27 and Py36.
    
    #### Also updates test's pinned `pants_version`

---
+++ ci.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import argparse
+import os
+import subprocess
+from contextlib import contextmanager
+from enum import Enum
+
+
+PANTS_INI = 'pants.ini'
+
+
+class PantsVersion(Enum):
+  unspecified = "unspecified"
+  config = "config"
+
+  def __str__(self):
+      return self.value
+
+
+def main() -> None:
+  args = create_parser().parse_args()
+  run_tests(test_pants_version=args.pants_version)
+
+
+def create_parser() -> argparse.ArgumentParser:
+  parser = argparse.ArgumentParser(description="Utility to run CI for the setup repo.")
+  parser.add_argument(
+      "--pants-version",
+      action="store",
+      type=PantsVersion,
+      choices=list(PantsVersion),
+      required=True,
+      help="Pants version to configure ./pants to use."
+  )
+  return parser
+
+
+def run_tests(*, test_pants_version: PantsVersion) -> None:
+  version_command = ["./pants", "--version"]
+  list_command = ["./pants", "list", "::"]
+  env_with_pantsd = {**os.environ, "PANTS_ENABLE_PANTSD": "True"}
+  with setup_pants_version(test_pants_version):
+    subprocess.run(version_command)
+    subprocess.run(list_command)
+    subprocess.run(version_command, env=env_with_pantsd)
+    subprocess.run(list_command, env=env_with_pantsd)
+
+
+@contextmanager
+def setup_pants_version(test_pants_version: PantsVersion):
+  """Modify pants.ini to allow the pants version to be unspecified or keep what was originally there."""
+  with open(PANTS_INI, 'r') as f:
+    original_pants_ini = list(f.readlines())
+  pants_version_already_specified = any(line.startswith("pants_version:") for line in original_pants_ini)
+  if test_pants_version == PantsVersion.config and not pants_version_already_specified:
+    raise ValueError("You requested to use the pants_version from pants.ini for this test, but pants.ini "
+                     "does not include a pants_version! Please update pants.ini and run again.")
+  if test_pants_version == PantsVersion.unspecified and pants_version_already_specified:
+    with open(PANTS_INI, 'w') as f:
+      # NB: we must not only remove the original definition of `pants_version`, but also
+      # any lines that make use of it, such as contrib packages pinning their version to `pants_version`.
+      f.writelines(line for line in original_pants_ini if "pants_version" not in line)
+  yield
+  with open(PANTS_INI, 'w') as f:
+    f.writelines(original_pants_ini)
+
+
+if __name__ == "__main__":
+  main()
