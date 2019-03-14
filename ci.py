843803dca6c940c86ced84fa99ec72b2bfa49dff
Eric Arellano <ericarellano@me.com>
2019-03-13 22:36:02 -0700

    Use configparser to dramatically simplify ci.py

--- ci.py
+++ ci.py
@@ -3,14 +3,15 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import argparse
+import configparser
 import os
 import subprocess
 from contextlib import contextmanager
 from enum import Enum
-from typing import List, Iterable, Optional
 
 
 PANTS_INI = 'pants.ini'
+GLOBAL_SECTION = "GLOBAL"
 
 
 class PantsVersion(Enum):
@@ -72,57 +73,45 @@ def run_tests(*, test_pants_version: PantsVersion, test_python_version: PythonVe
 @contextmanager
 def setup_pants_version(test_pants_version: PantsVersion):
   """Modify pants.ini to allow the pants version to be unspecified or keep what was originally there."""
-  original_lines = read_pants_ini()
+  original_config = read_config()
+  updated_config = read_config()
+  config_entry = "pants_version"
   if test_pants_version == PantsVersion.unspecified:
-    write_config_entry(entry_name="pants_version", entry_value=None)
+    updated_config.remove_option(GLOBAL_SECTION, config_entry)
+    # NB: We also remove plugins as they refer to the pants_version.
+    updated_config.remove_option(GLOBAL_SECTION, "plugins")
   elif test_pants_version == PantsVersion.config:
-    pants_version_defined = any(line.startswith("pants_version") for line in original_lines)
-    if not pants_version_defined:
+    if not config_entry in original_config[GLOBAL_SECTION]:
       raise ValueError("You requested to use the pants_version from pants.ini for this test, but pants.ini "
-                       "does not include a pants_version! Please update pants.ini and run again.")
+                       "does not include a pants_version!")
   yield
-  write_pants_ini(original_lines)
+  write_config(original_config)
 
 
 @contextmanager
 def setup_python_version(test_python_version: PythonVersion):
   """Modify pants.ini to allow the Python version to be unspecified or change to what was requested."""
-  original_lines = read_pants_ini()
-  requested_python_version = None if test_python_version == PythonVersion.unspecified else test_python_version.value
-  write_config_entry(entry_name="pants_runtime_python_version", entry_value=requested_python_version)
-  yield
-  write_pants_ini(original_lines)
-
-
-def read_pants_ini() -> List[str]:
-  with open(PANTS_INI, 'r') as f:
-    return list(f.readlines())
-
-
-def write_pants_ini(lines: Iterable[str]) -> None:
-  with open(PANTS_INI, 'w') as f:
-    f.writelines(lines)
-
-
-def write_config_entry(*, entry_name: str, entry_value: Optional[str]) -> None:
-  """Rewrite the entry in pants.ini to use the given value, entirely removing the entry if entry_value is None."""
-  original_lines = read_pants_ini()
-  entry_already_defined = any(line.startswith(entry_name) for line in original_lines)
-  if entry_value is None:
-    if not entry_already_defined:
-      new_lines = original_lines
-    else:
-      new_lines = (line for line in original_lines if entry_name not in line)
+  original_config = read_config()
+  updated_config = read_config()
+  config_entry = "pants_runtime_python_version"
+  if test_python_version == PythonVersion.unspecified:
+    updated_config.remove_option(GLOBAL_SECTION, config_entry)
   else:
-    if entry_already_defined:
-      new_lines = (line if not line.startswith(entry_name) else f"{entry_name}: {entry_value}" for line in original_lines)
-    else:
-      global_section_header_index = next((i for i, line in enumerate(original_lines) if "[GLOBAL]" in line), None)
-      if global_section_header_index is None:
-        raise ValueError("pants.ini is missing a [GLOBAL] section header. Please add this and run again.")
-      new_lines = original_lines
-      new_lines.insert(global_section_header_index + 1, f"{entry_name}: {entry_value}")
-  write_pants_ini(new_lines)
+    updated_config[GLOBAL_SECTION][config_entry] = test_python_version.value
+  write_config(updated_config)
+  yield
+  write_config(original_config)
+
+
+def read_config() -> configparser.ConfigParser:
+  cp = configparser.ConfigParser(delimiters={":"})
+  cp.read(PANTS_INI)
+  return cp
+
+
+def write_config(config: configparser.ConfigParser) -> None:
+  with open(PANTS_INI, 'w') as f:
+    config.write(f)
 
 
 if __name__ == "__main__":
