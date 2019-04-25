2241984f170c888efb9b00e5b944f25aa624950f
Eric Arellano <ericarellano@me.com>
2019-04-25 12:50:18 -0700

    Mark scripts and tests as Pants targets to unlock the benefits of Pants like using MyPy and isort

--- tests/test_first_time_install.py
+++ tests/test_first_time_install.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
@@ -11,15 +10,14 @@ https://github.com/pantsbuild/pants/blob/master/tests/python/pants_test/core_tas
 
 import re
 import subprocess
-import unittest
 
-from common import setup_pants_in_tmpdir, travis_section
+from test_base import TestBase
 
 
-class TestFirstTimeInstall(unittest.TestCase):
+class TestFirstTimeInstall(TestBase):
 
   def test_venv_name_uses_most_recent_stable_release(self) -> None:
-    with setup_pants_in_tmpdir() as tmpdir:
+    with self.setup_pants_in_tmpdir() as tmpdir:
       completed_process = subprocess.run(
         ["./pants", "--version"],
         check=True,
@@ -37,7 +35,7 @@ class TestFirstTimeInstall(unittest.TestCase):
       ))
 
   def test_only_bootstraps_the_first_time(self) -> None:
-    with setup_pants_in_tmpdir() as tmpdir:
+    with self.setup_pants_in_tmpdir() as tmpdir:
       first_run_pants_script_logging = subprocess.run(
         ["./pants", "--version"],
         check=True,
@@ -58,7 +56,3 @@ class TestFirstTimeInstall(unittest.TestCase):
       ).stderr
       # Check that stderr is falsy, i.e. there is no output.
       self.assertFalse(second_run_pants_script_logging)
-
-if __name__ == "__main__":
-  with travis_section("PantsFirstTimeInstall", "Testing first time install."):
-    unittest.main()
