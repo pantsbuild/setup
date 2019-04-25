f47e285c32fb1a76437f008304f5593cd6a3aefa
Eric Arellano <ericarellano@me.com>
2019-04-25 12:50:18 -0700

    Mark scripts and tests as Pants targets to unlock the benefits of Pants like using MyPy and isort

---
+++ tests/test_base.py
@@ -0,0 +1,38 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+import shutil
+import tempfile
+from contextlib import contextmanager
+from typing import Iterator
+from unittest import TestCase
+
+
+class TestBase(TestCase):
+  """A base class with useful utils for tests."""
+
+  @contextmanager
+  def copy_pants_into_tmpdir(self) -> Iterator[str]:
+    with tempfile.TemporaryDirectory() as tmpdir:
+      # NB: Unlike the install guide's instruction to curl the `./pants` script, we directly
+      # copy it to ensure we are using the branch's version of the script and to avoid
+      # network pings.
+      shutil.copy("pants", f"{tmpdir}/pants")
+      yield tmpdir
+
+  @contextmanager
+  def set_pants_cache_to_tmpdir(self) -> Iterator[None]:
+    with tempfile.TemporaryDirectory() as tmpdir:
+      original_env = os.environ.copy()
+      os.environ["PANTS_HOME"] = tmpdir
+      try:
+        yield
+      finally:
+        os.environ.clear()
+        os.environ.update(original_env)
+
+  @contextmanager
+  def setup_pants_in_tmpdir(self) -> Iterator[str]:
+    with self.set_pants_cache_to_tmpdir(), self.copy_pants_into_tmpdir() as buildroot_tmpdir:
+      yield buildroot_tmpdir
