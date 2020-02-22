ff22516346840b392e6481f90bd221aab10044e9
Eric Arellano <ericarellano@me.com>
2020-02-22 13:10:33 -0700

    Use pyenv to test Python interpreter resolution rather than `--pants-runtime-python-version` (#67)
    We previously used `--pants-runtime-python-version` to force Pants to use certain Python versions. This was not safe to keep doing, though, because we ended up removing this option.
    
    Instead, we can use Pyenv to get the same effect.
    
    This will allow us to start testing default Python interpreter resolution for Pants > 1.16.
    
    We remove Ubuntu 12.04 from CI to land this, as it has been EOL since April 2017 and causes issues with getting Pyenv to work properly.

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,14 +4,40 @@
 import configparser
 import os
 import shutil
+import subprocess
 import tempfile
 from contextlib import contextmanager
+from pathlib import Path
 from typing import Iterator, Optional
 from unittest import TestCase
 
 
 class TestBase(TestCase):
     """A base class with useful utils for tests."""
+
+    def setUp(self) -> None:
+        self.pyenv_bin: str
+        if "PYENV_BIN" in os.environ:
+            self.pyenv_bin = os.environ["PYENV_BIN"]
+        else:
+            pyenv_path = shutil.which("pyenv")
+            if pyenv_path is None:
+                raise ValueError(
+                    "Pyenv must be installed. The binary `pyenv` must either be discoverable from "
+                    "the `$PATH` or you must set the environment variable `PYENV_BIN`."
+                )
+            self.pyenv_bin = pyenv_path
+        if "PYENV_ROOT" not in os.environ:
+            raise ValueError(
+                "The environment variable `PYENV_ROOT` must be set. Please run "
+                "`export PYENV_ROOT=$(pyenv root)`."
+            )
+        self.pyenv_versions = subprocess.run(
+            [self.pyenv_bin, "versions", "--bare", "--skip-aliases"],
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+            check=True,
+        ).stdout.splitlines()
 
     @contextmanager
     def copy_pants_into_tmpdir(self) -> Iterator[str]:
@@ -38,22 +64,53 @@ class TestBase(TestCase):
         with self.set_pants_cache_to_tmpdir(), self.copy_pants_into_tmpdir() as buildroot_tmpdir:
             yield buildroot_tmpdir
 
-    def create_pants_ini(
-        self, *, parent_folder: str, pants_version: str, python_version: Optional[str] = None
-    ) -> None:
+    @contextmanager
+    def maybe_run_pyenv_local(
+        self, python_version: Optional[str], *, parent_folder: str
+    ) -> Iterator[None]:
+        if python_version is None:
+            yield
+            return
+
+        def is_compatible(pyenv_version: str) -> bool:
+            major, minor, _ = pyenv_version.split(".")
+            return f"{major}.{minor}" == python_version
+
+        compatible_pyenv_version = next(
+            (
+                pyenv_version
+                for pyenv_version in self.pyenv_versions
+                if is_compatible(pyenv_version)
+            ),
+            None,
+        )
+        if compatible_pyenv_version is None:
+            raise ValueError(
+                f"Python {python_version} is not installed via Pyenv. Please install with "
+                f"`pyenv install`. All installed versions: {', '.join(self.pyenv_versions)}."
+            )
+        subprocess.run(
+            [self.pyenv_bin, "local", compatible_pyenv_version], cwd=parent_folder, check=True
+        )
+        try:
+            yield
+        finally:
+            subprocess.run(
+                [self.pyenv_bin, "local", "--unset", compatible_pyenv_version],
+                cwd=parent_folder,
+                check=True,
+            )
+
+    @staticmethod
+    def create_pants_ini(*, parent_folder: str, pants_version: str) -> None:
         config = configparser.ConfigParser()
         config["GLOBAL"] = {
             "pants_version": pants_version,
             "plugins": "['pantsbuild.pants.contrib.go==%(pants_version)s']",
         }
-        # TODO: stop using `pants_runtime_python_versoin`, which is no longer allowed in Pants. Figure
-        # out how to set the Python version some other way, which could be via the $PYTHON
-        # env var (although that won't test that we resolve the default correctly..).
-        if python_version is not None:
-            config["GLOBAL"]["pants_runtime_python_version"] = python_version
         with open(f"{parent_folder}/pants.ini", "w") as f:
             config.write(f)
 
-    def create_dummy_build(self, *, parent_folder: str) -> None:
-        with open(f"{parent_folder}/BUILD", "w") as f:
-            f.write("target(name='test')\n")
+    @staticmethod
+    def create_dummy_build(*, parent_folder: str) -> None:
+        Path(parent_folder, "BUILD").write_text("target(name='test')\n")
