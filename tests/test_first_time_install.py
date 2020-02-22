3bb006e4a792ae83d7059ea76c0372ece7c1069d
Eric Arellano <ericarellano@me.com>
2020-02-22 11:10:17 -0700

    Use Tox instead of `build-support` scripts (#64)
    When we expanded our CI in May 2019, we took the approach of Pants of having multiple lint utility scripts in `build-support`, rather than Pex's approach of using `tox`. This was mostly due to my unfamiliarity with Tox.
    
    Beyond unnecessary custom code, another downside to this `build-support` approach is that it poses a chicken-and-the-egg problem when developing this repo. For example, for our tests and linters to run properly, `./pants` must work; however, the entire point of our linters and tests is to confirm that the script `./pants` works in the first place. Using `tox` allows us to run our tests and lints regardless of the condition of the `./pants` script.
    
    This also does the following:
    * Adds to CI macOS Mojave and Ubuntu 18.04.
    * Applies Black formatting
    * Uses Flake8 rather than the custom Pants checker

--- tests/test_first_time_install.py
+++ tests/test_first_time_install.py
@@ -15,42 +15,40 @@ from test_base import TestBase
 
 
 class TestFirstTimeInstall(TestBase):
+    def test_venv_name_uses_most_recent_stable_release(self) -> None:
+        with self.setup_pants_in_tmpdir() as tmpdir:
+            completed_process = subprocess.run(
+                ["./pants", "--version"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                encoding="utf-8",
+                cwd=tmpdir,
+            )
+            # Pip will resolve the most recent stable release, which will be the output of
+            # `./pants --version`.
+            downloaded_version = completed_process.stdout.strip()
+            assert re.search(
+                fr"virtual environment successfully created at .*/bootstrap.*/{downloaded_version}_py",
+                completed_process.stderr,
+                flags=re.MULTILINE,
+            )
 
-  def test_venv_name_uses_most_recent_stable_release(self) -> None:
-    with self.setup_pants_in_tmpdir() as tmpdir:
-      completed_process = subprocess.run(
-        ["./pants", "--version"],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-        cwd=tmpdir
-      )
-      # Pip will resolve the most recent stable release, which will be the output of
-      # `./pants --version`.
-      downloaded_version = completed_process.stdout.strip()
-      self.assertTrue(re.search(
-        fr"virtual environment successfully created at .*/bootstrap.*/{downloaded_version}_py",
-        completed_process.stderr, flags=re.MULTILINE
-      ))
-
-  def test_only_bootstraps_the_first_time(self) -> None:
-    with self.setup_pants_in_tmpdir() as tmpdir:
-      first_run_pants_script_logging = subprocess.run(
-        ["./pants", "--version"],
-        check=True,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-        cwd=tmpdir
-      ).stderr
-      # Check that stderr is truthy, i.e. there is output.
-      self.assertTrue(first_run_pants_script_logging)
-      second_run_pants_script_logging = subprocess.run(
-        ["./pants", "--version"],
-        check=True,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-        cwd=tmpdir
-      ).stderr
-      # Check that stderr is falsy, i.e. there is no output.
-      self.assertFalse(second_run_pants_script_logging)
+    def test_only_bootstraps_the_first_time(self) -> None:
+        with self.setup_pants_in_tmpdir() as tmpdir:
+            first_run_pants_script_logging = subprocess.run(
+                ["./pants", "--version"],
+                check=True,
+                stderr=subprocess.PIPE,
+                encoding="utf-8",
+                cwd=tmpdir,
+            ).stderr
+            assert first_run_pants_script_logging
+            second_run_pants_script_logging = subprocess.run(
+                ["./pants", "--version"],
+                check=True,
+                stderr=subprocess.PIPE,
+                encoding="utf-8",
+                cwd=tmpdir,
+            ).stderr
+            assert not second_run_pants_script_logging
