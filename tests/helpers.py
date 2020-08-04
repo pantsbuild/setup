5216e645a28fc77fd5bcb8b8b58cfea37737e42a
Benjy Weinberger <benjyw@gmail.com>
2020-08-04 16:28:32 -0700

    Lint

--- tests/helpers.py
+++ tests/helpers.py
@@ -11,14 +11,15 @@ import toml
 def create_pants_config(
     *, parent_folder: Path, pants_version: Optional[str], use_toml: bool = True
 ) -> None:
+    # NB: string comparison is not always correct for semvers, but it works in the
+    #  cases we care about for testing.
+    no_plugin = pants_version and pants_version >= "1.29"
     global_section = {}
     if pants_version:
         global_section["pants_version"] = pants_version
-    # NB: string comparison is not always correct for semvers, but it works in the
-    #  cases we care about for testing.
-    if pants_version < "1.29":
+    if not no_plugin:
         global_section["plugins"] = (
-            ["pantsbuild.pants.contrib.go==%(pants_version)s"]
+            ["pantsbuild.pants.contrib.go==%(pants_version)s"]  # type: ignore
             if pants_version
             else ["pantsbuild.pants.contrib.go"]
         )
@@ -34,6 +35,6 @@ def create_pants_config(
             toml.dump(config, f)
     else:
         cp = configparser.ConfigParser()
-        cp["GLOBAL"] = global_section  # type: ignore
+        cp["GLOBAL"] = global_section
         with (parent_folder / "pants.ini").open("w") as f:
             cp.write(f)
