909c5cf1e0b293f4740b73af93672ed2748ba681
Benjy Weinberger <benjyw@gmail.com>
2020-08-05 13:26:18 -0700

    Support running from an arbitrary master sha (#84)

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,7 +3,7 @@
 
 import configparser
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import toml
 
@@ -11,14 +11,19 @@ import toml
 def create_pants_config(
     *, parent_folder: Path, pants_version: Optional[str], use_toml: bool = True
 ) -> None:
-    global_section = (
-        {
-            "pants_version": pants_version,
-            "plugins": ["pantsbuild.pants.contrib.go==%(pants_version)s"],
-        }
-        if pants_version is not None
-        else {"plugins": ["pantsbuild.pants.contrib.go"]}
-    )
+    # NB: string comparison is not always correct for semvers, but it works in the
+    #  cases we care about for testing.
+    no_plugin = pants_version and pants_version >= "1.29"
+    global_section: Dict[str, Any] = {"backend_packages": ["pants.backend.python"]}
+    if pants_version:
+        global_section["pants_version"] = pants_version
+    if not no_plugin:
+        global_section["plugins"] = (
+            ["pantsbuild.pants.contrib.go==%(pants_version)s"]
+            if pants_version
+            else ["pantsbuild.pants.contrib.go"]
+        )
+
     if use_toml:
         config = {"GLOBAL": global_section}
         # TODO: string interpolation does not work for TOML when the value comes from the same
@@ -30,6 +35,6 @@ def create_pants_config(
             toml.dump(config, f)
     else:
         cp = configparser.ConfigParser()
-        cp["GLOBAL"] = global_section  # type: ignore
+        cp["GLOBAL"] = global_section
         with (parent_folder / "pants.ini").open("w") as f:
             cp.write(f)
--- tests/test_smoke_tests.py
+++ tests/test_smoke_tests.py
@@ -6,6 +6,7 @@
 import os
 import shutil
 import subprocess
+import textwrap
 from contextlib import contextmanager
 from dataclasses import dataclass
 from pathlib import Path
@@ -80,25 +81,46 @@ class SmokeTester:
             )
 
     def smoke_test(
-        self, *, pants_version: Optional[str], python_version: Optional[str], use_toml: bool = True
+        self,
+        *,
+        pants_version: Optional[str],
+        python_version: Optional[str],
+        use_toml: bool = True,
+        sha: Optional[str] = None,
     ) -> None:
+        env = {**os.environ}
+        if sha:
+            env["PANTS_SHA"] = sha
         version_command = ["./pants", "--version"]
         list_command = ["./pants", "list", "::"]
+        binary_command = ["./pants", "binary", "//:bin"]
         with self._maybe_run_pyenv_local(python_version):
             create_pants_config(
                 parent_folder=self.build_root, pants_version=pants_version, use_toml=use_toml
             )
-            (self.build_root / "BUILD").write_text("target(name='test')\n")
+            (self.build_root / "BUILD").write_text(
+                textwrap.dedent(
+                    """
+            target(name='test')
+
+            # To test that we can resolve these, esp. against custom shas.
+            pants_requirement(name='pantsreq')
+            python_binary(name='bin', dependencies=[':pantsreq'], entry_point='dummy')
+            """
+                )
+            )
 
             def run_command(command: List[str], **kwargs: Any) -> None:
                 subprocess.run(command, check=True, cwd=str(self.build_root), **kwargs)
 
-            run_command(version_command)
-            run_command(list_command)
+            run_command(version_command, env=env)
+            run_command(list_command, env=env)
+            run_command(binary_command, env=env)
             if "SKIP_PANTSD_TESTS" not in os.environ:
-                env_with_pantsd = {**os.environ, "PANTS_ENABLE_PANTSD": "True"}
+                env_with_pantsd = {**env, "PANTS_ENABLE_PANTSD": "True"}
                 run_command(version_command, env=env_with_pantsd)
                 run_command(list_command, env=env_with_pantsd)
+                run_command(binary_command, env=env_with_pantsd)
 
     def smoke_test_for_all_python_versions(
         self, *python_versions: str, pants_version: Optional[str], use_toml: bool = True
@@ -124,3 +146,14 @@ def test_pants_latest_stable(checker: SmokeTester) -> None:
 def test_pants_1_28(checker: SmokeTester) -> None:
     checker.smoke_test(python_version=None, pants_version="1.28.0")
     checker.smoke_test_for_all_python_versions("3.6", "3.7", "3.8", pants_version="1.28.0")
+
+
+def test_pants_2_0(checker: SmokeTester) -> None:
+    checker.smoke_test(python_version=None, pants_version="2.0.0.dev6")
+    checker.smoke_test_for_all_python_versions("3.6", "3.7", "3.8", pants_version="2.0.0.dev6")
+
+
+def test_pants_at_sha(checker: SmokeTester) -> None:
+    sha = "41ec94b758aac39c13f59e694fba5ed096a51ba9"
+    version = "2.0.0.dev6+git41ec94b7"
+    checker.smoke_test(python_version=None, pants_version=version, sha=sha)
