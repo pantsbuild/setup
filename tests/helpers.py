ed6829b80d5af8adad596912fae4b65712d9d542
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-09-25 10:26:35 -0700

    Require `pants_version` to be set (#86)
    We generally highly encourage users to use lockfiles because it's important for deterministic builds. But with the Pants script itself, we did not require this determinism, which could result in builds breaking randomly when we do new stable releases.
    
    Our docs already have been saying to set the `pants_version` before running `./pants`, so we don't need to update anything there.

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,36 +3,24 @@
 
 import configparser
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 import toml
 
 
-def create_pants_config(
-    *, parent_folder: Path, pants_version: Optional[str], use_toml: bool = True
-) -> None:
+def create_pants_config(*, parent_folder: Path, pants_version: str, use_toml: bool = True) -> None:
+    global_section: Dict[str, Any] = {
+        "pants_version": pants_version,
+        "backend_packages": ["pants.backend.python"],
+    }
     # NB: string comparison is not always correct for semvers, but it works in the
     #  cases we care about for testing.
-    no_plugin = pants_version and pants_version >= "1.29"
-    global_section: Dict[str, Any] = {"backend_packages": ["pants.backend.python"]}
-    if pants_version:
-        global_section["pants_version"] = pants_version
-    if not no_plugin:
-        global_section["plugins"] = (
-            ["pantsbuild.pants.contrib.go==%(pants_version)s"]
-            if pants_version
-            else ["pantsbuild.pants.contrib.go"]
-        )
+    if pants_version <= "1.28":
+        global_section["plugins"] = ["pantsbuild.pants.contrib.go==%(pants_version)s"]
 
     if use_toml:
-        config = {"GLOBAL": global_section}
-        # TODO: string interpolation does not work for TOML when the value comes from the same
-        #  section. This is fixed in Pants 1.26.0.dev1+.
-        if pants_version is not None:
-            config["GLOBAL"].pop("pants_version")
-            config["DEFAULT"] = {"pants_version": pants_version}
         with (parent_folder / "pants.toml").open("w") as f:
-            toml.dump(config, f)
+            toml.dump({"GLOBAL": global_section}, f)
     else:
         cp = configparser.ConfigParser()
         cp["GLOBAL"] = global_section
--- tests/test_first_time_install.py
+++ tests/test_first_time_install.py
@@ -12,26 +12,8 @@ from pathlib import Path
 from helpers import create_pants_config
 
 
-def test_venv_name_uses_most_recent_stable_release(build_root: Path) -> None:
-    result = subprocess.run(
-        ["./pants", "--version"],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-        cwd=str(build_root),
-    )
-    # Pip will resolve the most recent stable release, which will be the output of
-    # `./pants --version`.
-    downloaded_version = result.stdout.strip()
-    assert re.search(
-        fr"virtual environment successfully created at .*/bootstrap.*/{downloaded_version}_py",
-        result.stderr,
-        flags=re.MULTILINE,
-    )
-
-
 def test_only_bootstraps_the_first_time(build_root: Path) -> None:
+    create_pants_config(parent_folder=build_root, pants_version="1.30.1")
     first_run_pants_script_logging = subprocess.run(
         ["./pants", "--version"],
         check=True,
@@ -51,6 +33,7 @@ def test_only_bootstraps_the_first_time(build_root: Path) -> None:
 
 
 def test_relative_cache_locations_work(build_root: Path) -> None:
+    create_pants_config(parent_folder=build_root, pants_version="1.30.1")
     result = subprocess.run(
         ["./pants", "--version"],
         check=True,
@@ -76,7 +59,20 @@ def test_pants_1_22_and_earlier_fails(build_root: Path) -> None:
     assert "does not work with Pants <= 1.22.0" in result.stderr
 
 
+def test_pants_version_must_be_set(build_root: Path) -> None:
+    # We do not call `create_pants_config()`.
+    result = subprocess.run(
+        ["./pants", "--version"],
+        cwd=str(build_root),
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert result.returncode != 0
+    assert "Please explicitly specify the `pants_version` in your `pants.toml`" in result.stderr
+
+
 def test_python2_fails(build_root: Path) -> None:
+    create_pants_config(parent_folder=build_root, pants_version="1.30.1")
     result = subprocess.run(
         ["./pants", "--version"],
         cwd=str(build_root),
