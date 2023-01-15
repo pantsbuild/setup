79266f7603f1b228e453e7777fc0523239464012
Benjy Weinberger <benjyw@gmail.com>
2023-01-15 09:13:40 -0500

    A pantsup.sh script to easily install the latest scie-pants.

---
+++ tests/test_pantsup.py
@@ -0,0 +1,25 @@
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""Test the pantsup script."""
+
+import os
+import subprocess
+from pathlib import Path
+
+
+def test_installs_pants(tmp_path: Path) -> None:
+    cwd = os.getcwd()
+    proc = subprocess.run(
+        ["/bin/bash", os.path.join(cwd, "pantsup.sh")],
+        env={"HOME": str(tmp_path)},
+        capture_output=True,
+    )
+
+    assert proc.returncode == 0
+    assert b"Downloading scie-pants-" in proc.stderr
+    assert b"Verifying SHA256 of scie-pants-" in proc.stderr
+
+    bin_path = tmp_path / "bin" / "pants"
+    assert os.path.isfile(bin_path)
+    assert os.access(bin_path, os.X_OK)
