9ee2ce00c5072e1afae7355a4e69d411d5724dee
Huon Wilson <huon@exoflare.io>
2023-12-22 17:27:43 +1100

    blacken

--- tests/test_one_step_setup.py
+++ tests/test_one_step_setup.py
@@ -9,7 +9,6 @@ from pathlib import Path
 
 
 def test_runs_on_clean_directory(tmp_path: Path) -> None:
-
     cwd = os.getcwd()
 
     proc = subprocess.run(
@@ -23,7 +22,6 @@ def test_runs_on_clean_directory(tmp_path: Path) -> None:
 
 
 def test_pin_version(tmp_path: Path) -> None:
-
     cwd = os.getcwd()
     env = os.environ.copy()
     env["PANTS_VERSION"] = "2.11.0rc6"
@@ -46,7 +44,6 @@ def test_pin_version(tmp_path: Path) -> None:
 
 
 def test_fails_if_pants_script_present(tmp_path: Path) -> None:
-
     cwd = os.getcwd()
     pants_script = tmp_path / "pants"
     pants_script.touch()
@@ -60,7 +57,6 @@ def test_fails_if_pants_script_present(tmp_path: Path) -> None:
 
 
 def test_fails_if_pants_toml_present(tmp_path: Path) -> None:
-
     cwd = os.getcwd()
     pants_toml = tmp_path / "pants.toml"
     pants_toml.touch()
